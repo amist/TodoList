4aa11c1fd5c0413b5601b4ef178bca3c3340cc4f
amist <amistern0@gmail.com>
2014-02-24 19:16:16 +0200

    Create the project

---
+++ src/il/ac/huji/tipcalculator/TipCalculatorActivity.java
@@ -0,0 +1,47 @@
+package il.ac.huji.tipcalculator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TipCalculatorActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tip_calculator);
+		final Button calculateButton = (Button) findViewById(R.id.btnCalculate);
+		final TextView tipResultText = (TextView) findViewById(R.id.txtTipResult);
+		final EditText billAmountTxt = (EditText) findViewById(R.id.edtBillAmount);
+		final CheckBox roundTipCheck = (CheckBox) findViewById(R.id.chkRound);
+
+		calculateButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				String sumStr = billAmountTxt.getText().toString();
+				if (sumStr.length() == 0) {
+					tipResultText.setText("Please insert an amount");
+//					Log.d("myDebug", "going to return because of an empty string");
+					return;
+				}
+				float sum = Float.parseFloat(sumStr);
+				sum = sum * 12 / 100;
+				if (roundTipCheck.isChecked()) {
+					sum = Math.round(sum);
+				}
+				tipResultText.setText("Tip: $" + Float.toString(sum));
+			};
+		});
+	}
+
+}
