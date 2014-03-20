d7565c97e8321dd670c5f2b6310cef010219f1ed
amist <amistern0@gmail.com>
2014-03-20 19:20:27 +0200

    Style item menu activity

--- TodoList/src/il/ac/huji/todolist/ItemMenuActivity.java
+++ TodoList/src/il/ac/huji/todolist/ItemMenuActivity.java
@@ -97,7 +97,7 @@ public class ItemMenuActivity extends Activity implements OnClickListener {
 	}
 	
 	public boolean isCallText(String text) {
-		if (text.startsWith("call"))
+		if (text.toLowerCase().startsWith("call"))
 			return true;
 		return false;
 	}
